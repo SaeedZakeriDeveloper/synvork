8bbd8bda00d20591784e14a9f27749de414b99d2
TopLap <saeed.zakeri1364@gmail.com>
2023-09-26 02:19:20 +0330

    FGP(26/9/2023)

---
+++ public/js/main.js
@@ -0,0 +1,225 @@
+let menuBtn = document.querySelector('.menu-bar');
+const menu = document.querySelector('nav');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    menuBtn.classList.toggle('fa-times');
+});
+
+const navLi = document.querySelectorAll('nav ul li a');
+const sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        let sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop - 200) {
+            current = section.getAttribute('id');
+        }
+    });
+    navLi.forEach(li => {
+        li.classList.remove('active');
+        document.querySelector('nav ul li a[href*=' + current + ']').classList.add('active');
+    });
+});
+
+function openModal() {
+    var list = ['activity', 'firstName', 'lastName', 'email', 'description', 'phoneNumber', 'address'];
+    var invalidEmailBox = document.getElementById('invalidEmailBox');
+
+    list.forEach(x => {
+        var element = document.getElementById(x);
+        element.value = '';
+        element.className = '';
+        element.placeholder = '';
+        invalidEmailBox.style.visibility = 'hidden';
+    })
+    var modal = document.getElementById("requestFormModal");
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    var modal = document.getElementById("requestFormModal");
+    modal.style.display = "none";
+}
+
+// Function to show the form with a slide-down animation
+function showFormWithSlideDown() {
+    var modal = document.getElementById("requestFormModal");
+    modal.style.display = "block";
+}
+
+// Function to show the form with a slide-down animation
+function showFormWithSlideDown() {
+    var modal = document.getElementById("requestFormModal");
+    modal.style.display = "block";
+}
+
+// Function to close the modal (hide the form)
+function closeModal() {
+    var modal = document.getElementById("requestFormModal");
+    modal.style.display = "none";
+}
+
+function showSuccessMessage() {
+    var successModal = document.getElementById("successMessage");
+    successModal.style.display = "block";
+}
+
+// Function to close the success message
+function closeSuccessMessage() {
+    var successModal = document.getElementById("successMessage");
+    successModal.style.display = "none";
+}
+
+// Function to show the error message
+function showErrorMessage() {
+    var errorModal = document.getElementById("errorMessage");
+    errorModal.style.display = "block";
+}
+
+// Function to close the error message
+function closeErrorMessage() {
+    var errorModal = document.getElementById("errorMessage");
+    errorModal.style.display = "none";
+}
+
+function checkValidation(id) {
+    var element = document.getElementById(id);
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (id == 'email') {
+        if (element.value != '' && !element.value.match(validRegex)) {
+            element.className = 'invalid';
+            element.value = 'invalid';
+            return 0;
+        } else if (element.value != '' && element.value.match(validRegex)) {
+            element.className = 'valid';
+            return 1;
+        } else if (element.value == '') {
+            element.className = 'invalid';
+            element.value = 'required';
+            return 0;
+        }
+    } else if (element.value == '') {
+        element.className = 'invalid';
+        element.value = 'required';
+        return 0;
+    } else {
+        element.className = 'valid';
+
+        return 1;
+    }
+}
+
+function changeLanguage() {
+    let element = document.getElementById("langBtn");
+    let className = element.getAttribute("class");
+    if (className == 'franceBtn') {
+        element.classList.remove('franceBtn');
+        element.classList.add('unitedBtn');
+        window.location.href = "index.html";
+    } else if (className == 'unitedBtn') {
+        element.classList.remove('unitedBtn');
+        element.classList.add('franceBtn');
+        window.location.href = "index_en.html";
+    }
+}
+
+window.addEventListener('scroll', function () {
+    var animatedElements = document.querySelectorAll('.bounceToLeftBox');
+    for (var i = 0; i < animatedElements.length; i++) {
+        var element = animatedElements[i];
+        var positionFromTop = element.getBoundingClientRect().top;
+
+        if (positionFromTop - window.innerHeight <= 0) {
+            element.classList.add('bounceToLeft', 'anime');
+        }
+    }
+
+    var animatedElements = document.querySelectorAll('.bounceToRightBox');
+    for (var i = 0; i < animatedElements.length; i++) {
+        var element = animatedElements[i];
+        var positionFromTop = element.getBoundingClientRect().top;
+
+        if (positionFromTop - window.innerHeight <= 0) {
+            element.classList.add('bounceToRight', 'anime');
+        }
+    }
+
+    var animatedElements = document.querySelectorAll('.fadeInUpBox');
+    for (var i = 0; i < animatedElements.length; i++) {
+        var element = animatedElements[i];
+        var positionFromTop = element.getBoundingClientRect().top;
+
+        // if (positionFromTop - window.innerHeight <= 0) {
+        //     element.classList.add('fadeInUp');
+        // }
+        if (window.innerHeight - positionFromTop >= 270) {
+            element.classList.add('fadeInUp');
+        }
+    }
+
+    var animatedElements = document.querySelectorAll('.fadeInLeftBox');
+    for (var i = 0; i < animatedElements.length; i++) {
+        var element = animatedElements[i];
+        var positionFromTop = element.getBoundingClientRect().top;
+
+        // if (positionFromTop - window.innerHeight <= 0) {
+        //     element.classList.add('fadeInUp');
+        // }
+        if (window.innerHeight - positionFromTop >= 270) {
+            element.classList.add('fadeInLeft');
+        }
+    }
+
+    var animatedElements = document.querySelectorAll('.fadeInRightBox');
+    for (var i = 0; i < animatedElements.length; i++) {
+        var element = animatedElements[i];
+        var positionFromTop = element.getBoundingClientRect().top;
+
+        // if (positionFromTop - window.innerHeight <= 0) {
+        //     element.classList.add('fadeInUp');
+        // }
+        if (window.innerHeight - positionFromTop >= 270) {
+            element.classList.add('fadeInRight');
+        }
+    }
+});
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function playVideo() {
+    var video = document.getElementById("synvork-video");
+    var pauseSvg = document.getElementById("PauseSvg");
+    var playSvg = document.getElementById("PlaySvg");
+    if (video.paused) {
+        video.play();
+        playSvg.classList.replace('d-inline-block', 'd-none');
+        pauseSvg.classList.replace('d-none', 'd-inline-block');
+    } else {
+        video.pause();
+        pauseSvg.classList.replace('d-inline-block', 'd-none');
+        playSvg.classList.replace('d-none', 'd-inline-block');
+    }
+}
