e5a82af0bc7c6a3b9df167c1877cb74886466e14
TopLap <saeed.zakeri1364@gmail.com>
2023-10-06 02:29:50 +0330

    FGP(10/5/2023)

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 let menuBtn = document.querySelector('.menu-bar');
 const menu = document.querySelector('nav');
 
-menuBtn.addEventListener('click', () => {
+menuBtn?.addEventListener('click', () => {
     menu.classList.toggle('active');
     menuBtn.classList.toggle('fa-times');
 });
