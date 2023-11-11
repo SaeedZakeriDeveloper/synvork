386fd46939fa8e4548e1a0dd4a9aa9c8c6d1b21b
saeedzakeri1364 <saeed.zakeri1364@gmail.com>
2023-11-11 16:06:29 -0500

    saeed change language2

--- public/js/main.js
+++ public/js/main.js
@@ -249,12 +249,24 @@ document.addEventListener('DOMContentLoaded', function () {
         // Apply styles to the paragraph
         //   p.style.color = 'red';
 
-        p.textContent = "Our expertise encompasses refurbishing pharmaceutical machines" +
-            " like solid dosage form equipment injectable lines, and biopharmaceutical" +
-            " equipment. We excel in refurbishing machines such as blistering, tablet press," +
-            " vial filling, and purification systems. Our proficiency extends from solid dosage" +
-            " forms to up-stream and down-stream biopharmaceutical equipment."
-        // Replace the h1 with the new paragraph
+        // p.textContent = "Our expertise encompasses refurbishing pharmaceutical machines" +
+        //     " like solid dosage form equipment injectable lines, and biopharmaceutical" +
+        //     " equipment. We excel in refurbishing machines such as blistering, tablet press," +
+        //     " vial filling, and purification systems. Our proficiency extends from solid dosage" +
+        //     " forms to up-stream and down-stream biopharmaceutical equipment."
+
+
+
+        p.textContent = "Notre expertise englobe la rénovation de machines pharmaceutiques" +
+    " telles que l'équipement de formes posologiques solides, les lignes injectables et" +
+    " l'équipement biopharmaceutique. Nous excellons dans la rénovation de machines telles" +
+    " que l'emballage sous blister, la presse à comprimés, le remplissage de flacons et les" +
+    " systèmes de purification. Notre compétence s'étend des formes posologiques solides à" +
+    " l'équipement biopharmaceutique en amont et en aval.";
+
+
+
+
         p.classList.add('fade-in');
 
         Information_1.replaceWith(p);
@@ -264,9 +276,23 @@ document.addEventListener('DOMContentLoaded', function () {
     Information_2.addEventListener('click', function () {
         // Create a new paragraph element
         const p = document.createElement('p');
-        p.textContent = "Beyond pharmaceutical machinery, we extend our refurbishing capabilities to laboratory equipment.\n" +
-            "                From ovens to advanced freezers, centrifuges, and chromatography systems, including HPLC, UPLC, GC,\n" +
-            "                and Preparative HPLC, we breathe new life into vital lab instruments."
+        // p.textContent = "Beyond pharmaceutical machinery, we extend our refurbishing capabilities to laboratory equipment.\n" +
+        //     "                From ovens to advanced freezers, centrifuges, and chromatography systems, including HPLC, UPLC, GC,\n" +
+        //     "                and Preparative HPLC, we breathe new life into vital lab instruments."
+
+
+
+            p.textContent = "Au-delà des machines pharmaceutiques, nous étendons nos compétences de rénovation" +
+    " aux équipements de laboratoire. Des fours aux congélateurs avancés, en passant par" +
+    " les centrifugeuses et les systèmes de chromatographie, y compris HPLC, UPLC, GC et HPLC préparative," +
+    " nous redonnons vie à des instruments de laboratoire essentiels.";
+
+
+
+
+
+
+
 
         // Apply styles to the paragraph
         //   p.style.color = 'red';
@@ -282,10 +308,25 @@ document.addEventListener('DOMContentLoaded', function () {
     Information_3.addEventListener('click', function () {
         // Create a new paragraph element
         const p = document.createElement('p');
-        p.textContent = "At SYNVORK, quality is our cornerstone. We follow stringent refurbishing processes and quality\n" +
-            "                control measures to ensure that every machine and equipment we refurbish meets or exceeds industry\n" +
-            "                standards. Our team of experts, armed with extensive industry knowledge, is dedicated to\n" +
-            "                delivering reliable, high-performance machinery to our clients.";
+        // p.textContent = "At SYNVORK, quality is our cornerstone. We follow stringent refurbishing processes and quality\n" +
+        //     "                control measures to ensure that every machine and equipment we refurbish meets or exceeds industry\n" +
+        //     "                standards. Our team of experts, armed with extensive industry knowledge, is dedicated to\n" +
+        //     "                delivering reliable, high-performance machinery to our clients.";
+
+
+
+
+
+            p.textContent = "Chez SYNVORK, la qualité est notre pierre angulaire. Nous suivons des processus de rénovation" +
+            " rigoureux et des mesures de contrôle qualité pour garantir que chaque machine et équipement que" +
+            " nous rénovons répond ou dépasse les normes de l'industrie. Notre équipe d'experts, armée d'une" +
+            " connaissance approfondie de l'industrie, est dédiée à fournir des machines fiables et performantes à nos clients.";
+        
+
+
+
+
+
 
         // Apply styles to the paragraph
         //   p.style.color = 'red';
@@ -301,10 +342,19 @@ document.addEventListener('DOMContentLoaded', function () {
     Information_4.addEventListener('click', function () {
         // Create a new paragraph element
         const p = document.createElement('p');
-        p.textContent = "We prioritize sustainability and cost-effectiveness in the pharmaceutical industry by refurbishing\n" +
-            "                existing machinery, reducing waste, and lowering carbon footprint. Our refurbished equipment offers\n" +
-            "                a financially responsible alternative to buying new machines, allowing businesses to use their\n" +
-            "                resources more efficiently.";
+        // p.textContent = "We prioritize sustainability and cost-effectiveness in the pharmaceutical industry by refurbishing\n" +
+        //     "                existing machinery, reducing waste, and lowering carbon footprint. Our refurbished equipment offers\n" +
+        //     "                a financially responsible alternative to buying new machines, allowing businesses to use their\n" +
+        //     "                resources more efficiently.";
+
+
+
+
+            p.textContent = "Nous accordons la priorité à la durabilité et à la rentabilité dans l'industrie pharmaceutique" +
+    " en rénovant les machines existantes, réduisant les déchets et diminuant l'empreinte carbone." +
+    " Nos équipements rénovés offrent une alternative financièrement responsable à l'achat de nouvelles machines," +
+    " permettant aux entreprises d'utiliser leurs ressources de manière plus efficiente.";
+
 
         // Apply styles to the paragraph
         //   p.style.color = 'red';
@@ -322,8 +372,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const about_h2_1 = document.getElementById('about_h2_1');
     about_box_1.addEventListener('click', function () {
         const p = document.createElement('p');
-        p.textContent = "Refurbished machinery provides reliable production processes through rigorous testing and\n" +
-            "                    inspections, increasing client confidence in equipment surpassing industry standards.";
+        // p.textContent = "Refurbished machinery provides reliable production processes through rigorous testing and\n" +
+        //     "                    inspections, increasing client confidence in equipment surpassing industry standards.";
+
+            p.textContent = "Les machines rénovées offrent des processus de production fiables grâce à des tests" +
+    " et des inspections rigoureux, renforçant la confiance des clients dans des équipements dépassant" +
+    " les normes de l'industrie.";
+
+
         about_box_1.style.animation = 'rotation 1s ease-in-out';
         about_h2_1.replaceWith(p);
     });
@@ -332,8 +388,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const about_h2_2 = document.getElementById('about_h2_2');
     about_box_2.addEventListener('click', function () {
         const p = document.createElement('p');
-        p.textContent = "Refurbished pharmaceutical machinery offers a cost-effective option with excellent performance,\n" +
-            "                    saving on the high capital investment required for new equipment.";
+        // p.textContent = "Refurbished pharmaceutical machinery offers a cost-effective option with excellent performance,\n" +
+        //     "                    saving on the high capital investment required for new equipment.";
+
+
+            p.textContent = "Les machines pharmaceutiques rénovées offrent une option économique avec une excellente performance," +
+            " permettant d'économiser sur l'investissement en capital élevé requis pour de nouveaux équipements.";
+        
+
         about_box_2.style.animation = 'rotation 1s ease-in-out';
         about_h2_2.replaceWith(p);
     });
@@ -342,9 +404,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const about_h2_3 = document.getElementById('about_h2_3');
     about_box_3.addEventListener('click', function () {
         const p = document.createElement('p');
-        p.textContent = "SYNVORK specializes in customizing refurbished machines to fit unique production needs,\n" +
-            "                    seamlessly\n" +
-            "                    integrating them into existing production lines.";
+        // p.textContent = "SYNVORK specializes in customizing refurbished machines to fit unique production needs,\n" +
+        //     "                    seamlessly\n" +
+        //     "                    integrating them into existing production lines.";
+
+
+            p.textContent = "SYNVORK se spécialise dans la personnalisation de machines rénovées pour répondre aux besoins" +
+    " de production uniques, les intégrant de manière transparente dans les lignes de production existantes.";
+
         about_box_3.style.animation = 'rotation 1s ease-in-out';
         about_h2_3.replaceWith(p);
     });
@@ -353,8 +420,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const about_h2_4 = document.getElementById('about_h2_4');
     about_box_4.addEventListener('click', function () {
         const p = document.createElement('p');
-        p.textContent = "Refurbished machines in pharmaceutical production save time and money by allowing businesses to\n" +
-            "                    quickly meet market demands, making them more efficient than new machines.";
+        // p.textContent = "Refurbished machines in pharmaceutical production save time and money by allowing businesses to\n" +
+        //     "                    quickly meet market demands, making them more efficient than new machines.";
+
+            p.textContent = "Les machines rénovées dans la production pharmaceutique permettent d'économiser du temps et de l'argent" +
+    " en permettant aux entreprises de répondre rapidement aux demandes du marché, les rendant plus efficaces que les" +
+    " nouvelles machines.";
+
         about_box_4.style.animation = 'rotation 1s ease-in-out';
         about_h2_4.replaceWith(p);
     });
